9b004315f7089fe697a2154313ee28ec53cd1763
mnkr9870 <nanda.mangunuri@gmail.com>
2024-07-29 10:58:18 -0400

    wokrflow4

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -14,9 +14,9 @@ def initiate():
     for data in car_make_data:
         car_make_instances.append(
             CarMake.objects.create(name=data['name'],
-            description=data['description']))
-
-
+            description=data['description'])
+            )
+            
     # Create CarModel instances with the
     # corresponding CarMake instances
     car_model_data = [
@@ -70,6 +70,6 @@ def initiate():
 
     for data in car_model_data:
         CarModel.objects.create(
-            name=data['name'], 
-            car_make=data['car_make'], 
+            name=data['name'],
+            car_make=data['car_make'],
             type=data['type'], year=data['year'])
