83feb94e3d5e928de410a4ef80b007dc799d6130
mnkr9870 <nanda.mangunuri@gmail.com>
2024-07-29 10:32:22 -0400

    workflow1

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -39,7 +39,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://nandamangunu-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
     'https://nandamangunu-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
     'https://nandamangunu-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-    'https://nandamangunu-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    """https://nandamangunu-3030.
+    theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""
     ]
 
 REST_FRAMEWORK = {
@@ -105,7 +106,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        """django.contrib.auth.password_validation.
+        UserAttributeSimilarityValidator""",
     },
     {
         'NAME':
